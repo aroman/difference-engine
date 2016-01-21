529f86485382e6bc3c4273e2c16ab9d098184292
Avi Romanoff <avi@romanoff.me>
2016-01-20 22:02:17 -0500

    initial commit

---
+++ README.txt
@@ -0,0 +1,73 @@
+# How to execute the program
+
+This program is written in Python. It was written and tested against the latest stable version, 3.5, but it should work fine with any version >= 3.2.
+
+To run the program, simply pass the `engine.py` file to your Python 3 interpreter, e.g. `python3 engine.py`. The specific interpreter alias is machine-dependent, and you'll need to know what it is for your machine.
+
+# Basic usage
+
+Follow the in-console prompts.
+
+Example:
+
+```
+$ python3 engine.py
+Difference Engine Simulator (degree = 8)
+f(x): x^2 + 3x + 4
+number of cranks: 3
+Δ0: x^2 + 3x + 4
+Δ1: 2x + 4
+Δ2: 2
+0   0   0   0   0   0   0   0
+0   0   0   0   0   0   0   0
+2   1   0   0   0   0   0   0
+2   0   2   0   0   0   0   0
+```
+
+# Advanced use (embellishments)
+
+## Polynomial input
+
+As you've noticed from the basic usage section, you do not need to compute the
+difference functions by hand to calculate the initial values -- you can just
+type in the polynomial f(x) function by hand. The parser supports any
+single-variable polynomial with no negative terms.
+
+Valid input examples:
+
+f(x): 8x^4 + x^2 + 2x + 19
+f(x): 8t^4 + t^2 + 2t + 19 -> you need not use x, but they'll be treated as such
+f(x): 8x4 + x2 + 2x + 19 -> carets may be omitted
+f(x): x^18 -> only useful if you've increased the degree cap, see below
+f(x): 100000
+
+Invalid input examples:
+
+f(x): hello -> only polynomials are supported
+f(x): cos(x) -> only polynomials are supported
+f(x): -10x^2 -> negative terms not supported
+f(x): x^2 - 1 -> negative terms not supported
+
+## Higher-degree polynomials (increased columns)
+
+The engine supports arbitrary polynomials up to any degree. By default, it
+only supports up to 8th-degree polynomials, as per the writeup. However, you can
+set this limit to any integer you want, by passing that number as a command-line argument.
+
+Example:
+
+```
+$ python3 engine.py 15
+Difference Engine Simulator (degree = 15)
+f(x): 8x4 + x2 + 2x + 19
+number of cranks: 10
+Δ0: 8x^4 + x^2 + 2x + 19
+Δ1: 32x^3 + 48x^2 + 34x + 11
+Δ2: 96x^2 + 192x + 114
+Δ3: 192x + 288
+Δ4: 192
+0   7   1   2   0   0   0   0   0   0   0   0   0   0   0
+1   1   6   2   1   0   0   0   0   0   0   0   0   0   0
+3   5   3   0   9   0   0   0   0   0   0   0   0   0   0
+9   1   4   8   2   0   0   0   0   0   0   0   0   0   0
+```
---
+++ engine.py
@@ -0,0 +1,98 @@
+# Copyright (C) Avi Romanoff <avi@romanoff.me>
+
+import re
+from sys import argv
+from functools import partial, reduce
+from itertools import repeat, chain, groupby
+from math import factorial as fac
+from collections import namedtuple
+
+DEGREE = int(argv[1]) if (len(argv) > 1 and argv[1].isdigit()) else 8
+flatten = lambda i: chain.from_iterable(i)
+extend = partial(lambda n, xs: (xs + [0] * n)[:n], DEGREE)
+Term = namedtuple('PolynomialTerm', ['coefficient', 'exponent'])
+
+def main():
+    print('Difference Engine Simulator (degree = %i)' % DEGREE)
+    terms = parsePolynomial(input('f(x): '))
+    cranks = int(input('number of cranks: '))
+
+    differences =  list(map(partial(apply, difference, terms), range(DEGREE)))
+    coefficients = list(map(partial(solve, 0), differences))
+
+    for i, d in enumerate(differences):
+        if not d: continue
+        d = sorted(d, key=lambda t: -t.exponent)
+        print('Δ{}: {}'.format(i, ' + '.join(map(rep, d))))
+
+    for _ in range(cranks):
+        coefficients = crank(coefficients)
+    display(extend(coefficients))
+
+def rep(t):
+    c = t.coefficient if t.coefficient > 1 else ''
+    if t.exponent == 0: return '{}'.format(c)
+    elif t.exponent == 1: return '{}x'.format(c)
+    else: return '{}x^{}'.format(c, t.exponent)
+
+def apply(f, x, n):
+    for _ in range(n):
+        x = f(x)
+    return x
+
+def simplify(ts):
+    c = lambda t: t.coefficient
+    e = lambda t: t.exponent
+    combine = lambda a, b: Term(a.coefficient + b.coefficient, a.exponent)
+    return list(filter(c,
+        (reduce(combine, g) for _, g in groupby(sorted(ts, key=e), key=e))
+    ))
+
+def solve(x, ts):
+    return sum(map(lambda t: t.coefficient * x ** t.exponent, ts))
+
+# f(x) => f(x+1) - f(x)
+def difference(ts):
+    return simplify(chain(flatten(map(successorTerm, ts)), map(negate, ts)))
+
+def negate(t):
+    return Term(coefficient=-t.coefficient, exponent=t.exponent)
+
+# f(x) => f(x+1)
+def successorTerm(t):
+    C = lambda n, k: round(fac(n) / (fac(k) * fac(n-k)))
+    if t.exponent == 0: return (t,)
+    if t.exponent == 1: return (t, Term(t.coefficient, 0))
+    elif t.exponent > 1: return map(
+        lambda z: Term(t.coefficient * C(z[0], z[1]), z[1]),
+        zip(
+            repeat(t.exponent, t.exponent + 1), # n
+            range(t.exponent + 1) # k
+        )
+    )
+
+def crank(values):
+    pairs = zip(values, values[1:])
+    return list(map(sum, pairs))
+
+def display(values, rows=4):
+    digits = lambda i: list(map(int, reversed(str(i))))
+    cols = [digits(v) for v in values]
+    for row in reversed(range(rows)):
+        for col in cols:
+            print(col[row] if row < len(col) else '0', end='   ')
+        print()
+
+def parseTerm(s):
+    if s.isdigit(): return Term(coefficient=int(s), exponent=0)
+    matches = re.match('(\d*)\w?\^?(\d*)', s.strip())
+    return Term(
+        coefficient=int(matches.group(1) or 1),
+        exponent=int(matches.group(2) or 1)
+    )
+
+def parsePolynomial(s):
+    terms = map(parseTerm, [c.strip() for c in s.split('+')])
+    return sorted(terms, key=lambda t: -t.exponent)
+
+main()
